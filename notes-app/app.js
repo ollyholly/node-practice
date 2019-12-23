d29777743ff126525aa1bd3a4a07a81044aec32a
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-23 15:49:43 +0000

    Practice color pringing with chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,8 @@
-const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes.js");
 
-// fs.writeFileSync("notes.txt", "This file was created by Node JS\n");
-// fs.appendFileSync("notes.txt", "Hello World!");
-
-const message = getNotes();
-
-console.log(message);
-console.log(validator.isEmail("mr.gipsy@gmail.com"));
-console.log(validator.isURL("htt//handdrawngoods.com"));
+console.log(
+  chalk.green.bold("To Be ") +
+    chalk.blueBright.dim("or ") +
+    chalk.redBright.underline("Not To Be")
+);
