a278e9faf12790a584afe2cf78aae91bf17f8659
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-23 15:32:12 +0000

    Practice module export

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const getNotes = require("./notes.js");
 
-fs.writeFileSync("notes.txt", "This file was created by Node JS\n");
-fs.appendFileSync("notes.txt", "Hello World!");
+// fs.writeFileSync("notes.txt", "This file was created by Node JS\n");
+// fs.appendFileSync("notes.txt", "Hello World!");
+
+const message = getNotes();
+
+console.log(message);
