fe2bb3842206e6469157f5ee84fa54dbfa940ba2
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-23 15:42:09 +0000

    Practice using validator package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const validator = require("validator");
 const getNotes = require("./notes.js");
 
 // fs.writeFileSync("notes.txt", "This file was created by Node JS\n");
@@ -7,3 +7,5 @@ const getNotes = require("./notes.js");
 const message = getNotes();
 
 console.log(message);
+console.log(validator.isEmail("mr.gipsy@gmail.com"));
+console.log(validator.isURL("htt//handdrawngoods.com"));
