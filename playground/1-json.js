0d09d6a7bd49b5d491c6cf4a2c798cdd99ac398a
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-24 20:52:30 +0000

    Practice reading JSON data from a file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,15 +1,20 @@
 const fs = require("fs");
 
-const book = {
-  title: "Ego is the enemy",
-  author: "Ryan Holiday"
-};
+const bookData = fs.readFileSync("1-json.json").toString();
+console.log(bookData.toString());
 
-const bookJSON = JSON.stringify(book);
+const parsedData = JSON.parse(bookData);
 
-fs.writeFileSync("1-json.json", bookJSON);
+// const book = {
+//   title: "Ego is the enemy",
+//   author: "Ryan Holiday"
+// };
+
+// const bookJSON = JSON.stringify(book);
+
+// fs.writeFileSync("1-json.json", bookJSON);
 
 // console.log(bookJSON);
 
 // const parsedData = JSON.parse(bookJSON);
-// console.log(parsedData.author);
+console.log(parsedData.author);
