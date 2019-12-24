a3af074eb0afbc08cfd125367061f1f0f9fcbe53
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-24 20:45:13 +0000

    Practice writing JSON data to file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,15 @@
+const fs = require("fs");
+
 const book = {
   title: "Ego is the enemy",
   author: "Ryan Holiday"
 };
 
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
 
-const parsedData = JSON.parse(bookJSON);
-console.log(parsedData.author);
+fs.writeFileSync("1-json.json", bookJSON);
+
+// console.log(bookJSON);
+
+// const parsedData = JSON.parse(bookJSON);
+// console.log(parsedData.author);
