2a31ad2ef437c4a10778a42b25a2c4cf99e3e0b1
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-25 14:43:51 +0000

    Add error handling for the API calls.

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,13 +6,18 @@ const weather_key = process.env.DARKSKY_TOKEN;
 const url = `https://api.darksky.net/forecast/${weather_key}/37.8267,-122.4233?units=si`;
 
 request({ url: url, json: true }, (error, response) => {
-  // const data = JSON.parse(response.body);
-  const summary = response.body.daily.data[0].summary;
-  const temperature = response.body.currently.temperature;
-  const rainChance = response.body.currently.precipProbability * 100;
-  console.log(
-    `${summary} It is currently ${temperature} degrees out. There is ${rainChance}% chance of rain.`
-  );
+  if (error) {
+    console.log("Unable to connect to weather services!");
+  } else if (response.body.error) {
+    console.log("Error: ", response.body.error);
+  } else {
+    const summary = response.body.daily.data[0].summary;
+    const temperature = response.body.currently.temperature;
+    const rainChance = response.body.currently.precipProbability * 100;
+    console.log(
+      `${summary} It is currently ${temperature} degrees out. There is ${rainChance}% chance of rain.`
+    );
+  }
 });
 
 const location = "London";
@@ -20,8 +25,16 @@ const map_key = process.env.MAPBOX_TOKEN;
 const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${map_key}&limit=1`;
 
 request({ url: geoUrl, json: true }, (error, response) => {
-  const destination = response.body.features[0].place_name;
-  const latitude = response.body.features[0].center[0];
-  const longitude = response.body.features[0].center[1];
-  console.log(`${destination}\nLatitude: ${latitude}\nLongitude: ${longitude}`);
+  if (error) {
+    console.log("Can not connect to geocoding services!");
+  } else if (response.body.features.length === 0) {
+    console.log("Can not find this location, try other query");
+  } else {
+    const destination = response.body.features[0].place_name;
+    const latitude = response.body.features[0].center[0];
+    const longitude = response.body.features[0].center[1];
+    console.log(
+      `${destination}\nLatitude: ${latitude}\nLongitude: ${longitude}`
+    );
+  }
 });
