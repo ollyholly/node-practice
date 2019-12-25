bbf103ecf65bc9b61fca38d777a98921adc95110
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-25 17:29:19 +0000

    Refactor forecast method with callback in a separate module

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,14 @@
 const request = require("request");
 const dotenv = require("dotenv");
 const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 dotenv.config();
-// const weather_key = process.env.DARKSKY_TOKEN;
-// const url = `https://api.darksky.net/forecast/${weather_key}/37.8267,-122.4233?units=si`;
 
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to weather services!");
-//   } else if (response.body.error) {
-//     console.log("Error: ", response.body.error);
-//   } else {
-//     const summary = response.body.daily.data[0].summary;
-//     const temperature = response.body.currently.temperature;
-//     const rainChance = response.body.currently.precipProbability * 100;
-//     console.log(
-//       `${summary} It is currently ${temperature} degrees out. There is ${rainChance}% chance of rain.`
-//     );
-//   }
-// });
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
+});
 
 geocode("London", (error, data) => {
   if (error) {
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require("request");
+const dotenv = require("dotenv");
+
+const forecast = (lat, lon, callback) => {
+  const weather_key = process.env.DARKSKY_TOKEN;
+  const url = `https://api.darksky.net/forecast/${weather_key}/${lat},${lon}?units=si`;
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather services!");
+    } else if (response.body.error) {
+      callback("Error: ", response.body.error);
+    } else {
+      const summary = response.body.daily.data[0].summary;
+      const temperature = response.body.currently.temperature;
+      const rainChance = response.body.currently.precipProbability * 100;
+      callback(
+        undefined,
+        `${summary} It is currently ${temperature} degrees out. There is ${rainChance}% chance of rain.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
