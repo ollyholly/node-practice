7c2afcbda9869054d858c2dd5573b219e2269dcb
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-25 13:48:15 +0000

    Format request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,13 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/7029036e0b6dc750485ef578a87df769/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/7029036e0b6dc750485ef578a87df769/37.8267,-122.4233?units=si";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+  // const data = JSON.parse(response.body);
+  const temperature = response.body.currently.temperature;
+  const rainChance = response.body.currently.precipProbability * 100;
+  console.log(
+    `It is currently ${temperature} degrees out. There is ${rainChance}% chance of rain.`
+  );
 });
