f2a2d554b3e208ff3dfc3a6ed05f075f6065af6f
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-28 13:53:48 +0000

    Setup public folder and configure server to serve files from this folder

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,31 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.send("Hello world!");
+});
+
+app.get("/help", (req, res) => {
+  res.send([
+    { name: "Olly", age: "35" },
+    { name: "Holly", age: "32" }
+  ]);
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About us</h1>");
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    location: "London",
+    weather: { temperature: 20, rainChance: 0.1 }
+  });
 });
 
 app.listen(3000, () => {
