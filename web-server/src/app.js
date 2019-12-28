8f29e401a558c4e72a129ac9a5a148c6192b0c2e
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-28 15:30:17 +0000

    Add partials for header and footer

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,21 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.use(express.static(publicDirectoryPath));
+// define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
   res.render("index", {
