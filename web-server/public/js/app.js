550f3592def6685c4f353f0ec4f22a905f674e0a
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-29 17:48:14 +0000

    Add frontend fetch call to weather end point.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,13 @@
 console.log("frontend script loaded");
+
+fetch("http://localhost:3000/weather?address=boston")
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(data.destination);
+    console.log(data.weatherForecast);
+  })
+  .catch(e => {
+    console.log(e);
+  });
