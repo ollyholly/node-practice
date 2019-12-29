93b256c09b0cef1e90ee7162bf5b01806940b9bc
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-29 18:01:29 +0000

    Add form to get the location and send a fetch call with this location

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,25 @@
 console.log("frontend script loaded");
 
-fetch("http://localhost:3000/weather?address=boston")
-  .then(res => {
-    return res.json();
-  })
-  .then(data => {
-    console.log(data.destination);
-    console.log(data.weatherForecast);
-  })
-  .catch(e => {
-    console.log(e);
-  });
+const weatherCall = location => {
+  fetch("http://localhost:3000/weather?address=" + location)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data.destination);
+      console.log(data.weatherForecast);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+};
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const location = search.value;
+  weatherCall(location);
+});
