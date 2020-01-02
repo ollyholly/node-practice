ad5252ac0415cfc507a888873679d9076a6a1832
Olly Holovchenko <oleh.holo@gmail.com>
2020-01-02 12:55:06 +0000

    Insert multiple documents into the database

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -15,9 +15,61 @@ MongoClient.connect(
     console.log("Connected correctly!");
 
     const db = client.db(databaseName);
-    db.collection("users").insertOne({
-      name: "Olly",
-      age: 35
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Olly",
+    //     age: 35
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       console.log("Unable to insert a user!");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jess",
+    //       age: 31
+    //     },
+    //     {
+    //       name: "Bob",
+    //       age: 25
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert users!");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Wash dished",
+          completed: true
+        },
+        {
+          description: "Water plants",
+          completed: true
+        },
+        {
+          description: "Feed the dinos",
+          completed: false
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Can not insert the tasks");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
