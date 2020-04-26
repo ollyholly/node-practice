ade6208d47a532551de895f43c6e2284aa34e513
Olly Holovchenko <olly@tray.io>
2020-04-26 11:28:35 +0100

    Restart working on Task manager.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -15,13 +15,18 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users")
-      .deleteMany({ age: 35 })
-      .then(result => {
-        console.log(result);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    db.collection('users').insertOne({
+      name: 'Olly',
+      age: 35
+    })
+
+    // db.collection("users")
+    //   .deleteMany({ age: 35 })
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
   }
 );
